c23c65eecebb82b6662e1507467ca21db054f70e
Franktrue <807615827@qq.com>
2021-08-12 19:21:15 +0800

    添加客户端

---
+++ demo-cli/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	pb "github.com/franktrue/microshop/services/demo/proto/demo"
+	"github.com/micro/go-micro/v2"
+	"log"
+)
+
+func main() {
+	service := micro.NewService(
+		micro.Name("services.demo-cli"),
+	)
+	service.Init()
+
+	client := pb.NewDemoServiceClient("services.demo", service.Client())
+	rsp, err := client.SayHello(context.TODO(), &pb.DemoRequest{Name: "Franktrue"})
+	if err != nil {
+		log.Fatalf("服务调用失败：%v", err)
+		return
+	}
+	log.Println(rsp.Text)
+}
