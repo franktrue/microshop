9d2c66956cf6dff1534002dfc588ce74e3b4b74f
Franktrue <807615827@qq.com>
2021-08-13 08:44:40 +0800

    修改目录结构

---
+++ demo-service/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	pb "github.com/franktrue/microshop/services/demo-service/proto/demo"
+	"github.com/micro/go-micro/v2"
+	"log"
+)
+
+type DemoServiceHandler struct{}
+
+func (d *DemoServiceHandler) SayHello(ctx context.Context, req *pb.DemoRequest, rsp *pb.DemoResponse) error {
+	rsp.Text = "你好，" + req.Name
+	return nil
+}
+
+func main() {
+	// 注册服务名必须和demo.proto中的package声明一致
+	service := micro.NewService(
+		micro.Name("services.demo"),
+	)
+
+	service.Init()
+	pb.RegisterDemoServiceHandler(service.Server(), &DemoServiceHandler{})
+	if err := service.Run(); err != nil {
+		log.Fatalf("服务启动失败: %v", err)
+	}
+}
